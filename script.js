8db45579f373b30e1065335d1076424afb7b8688
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-02-04 23:09:08 +0530

    mssg

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+// 1-closures
+// function outer() {
+//   let count = 0;
+//   return function inner() {
+//     count++;
+//     return count;
+//   };
+// }
+// const counter = outer();   // outer() runs once
+// console.log(counter());   // 1
+// console.log(counter());   // 2
+// console.log(counter());   // 3
+// console.log(counter());   // 4
+
+// ---------------------------------------------
+
+// 2-this keyword
+// const user = {
+//   name: "Alex",
+//   greet() {
+//     console.log(this.name);
+//           return this.name;
+
+//   }
+// };
+// user.greet()
+// const display = user.greet()
+// console.log(display)
+
+const user = {
+  name: "ayush",
+  age: 20,
+  hii() {
+    console.log(this.name); //print
+    // return this.name
+  },
+};
+console.log("coming from function---", user.hii());
